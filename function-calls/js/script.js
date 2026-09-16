6132e913cde3a442ff08232094fae97d6be69fcb
bethtempletonixd <bethtempleton99@gmail.com>
2026-09-16 19:26:24 -0400

    drawingRect

--- function-calls/js/script.js
+++ function-calls/js/script.js
@@ -12,7 +12,7 @@
  * OH LOOK I DIDN'T DESCRIBE SETUP!!
 */
 function setup() {
-
+createCanvas(400, 400); //creating a canvas to draw on
 }
 
 
@@ -20,5 +20,6 @@ function setup() {
  * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
 */
 function draw() {
-
+background(255);//background is white 
+rect(width/2, height/2, 100, 300);//draws a rectangle in middle of screen 
 }
