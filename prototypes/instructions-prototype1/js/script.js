7b85ac106f8f9e5d61ea1a1e92d4813baae5c8f5
bethtempletonixd <bethtempleton99@gmail.com>
2026-09-22 12:31:34 -0400

    commentedCode

--- prototypes/instructions-prototype1/js/script.js
+++ prototypes/instructions-prototype1/js/script.js
@@ -1,5 +1,5 @@
-const grid = 50;
-const inset = 0 + grid/2;
+const grid = 50; //how spaced the shapes are
+const inset = 0 + grid/2;//inset from outside of canvas
 
 function setup() {
 createCanvas(500, 500);
@@ -11,8 +11,8 @@ background(0);
 function draw() {
 fill(255);
 rectMode(CENTER);
-for (let y = 0; y < 10; y++){
-    for(let x = 0; x < 10; x++){
+for (let y = 0; y < 10; y++){//draw y axis of 10 squares
+    for(let x = 0; x < 10; x++){//draw x axis of 10 squares
 rect(inset + x * grid, inset + y * grid, 10, 10);
 }
 }
