21107a825cc34633dd931ac3d673bad21820fd6a
bethtempletonixd <bethtempleton99@gmail.com>
2026-09-22 16:31:48 -0400

    more colourful

--- prototypes/instructions-prototype2/js/script.js
+++ prototypes/instructions-prototype2/js/script.js
@@ -3,15 +3,17 @@ function setup() {
 createCanvas(500, 500);
 background(0);
 noLoop();
+colorMode(HSB);
 }
 
 
 function draw() {
 background(0);
-for (let i = 0; i < 150; i++){
+for (let i = 0; i < 1000; i++){
     noStroke();
-    fill(random(255)); //different shades of white
-rect(random(width), random(height), 10, 30);
+    fill(random(300), 180, 200); //different shades of white
+    rotate(random(TWO_PI));
+rect(random(width), random(height), 20, 60);
 }
 }
 
