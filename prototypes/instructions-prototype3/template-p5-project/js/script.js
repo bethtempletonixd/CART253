6c8be6f3971206fa7458898b37ea9f77ad141050
bethtempletonixd <bethtempleton99@gmail.com>
2026-09-22 16:20:31 -0400

    commented code

--- prototypes/instructions-prototype3/template-p5-project/js/script.js
+++ prototypes/instructions-prototype3/template-p5-project/js/script.js
@@ -11,15 +11,15 @@ noLoop();
 
 function draw() {
     background(0);
-    squiggle01();
-    squiggle02();
+    squiggle01();//colourful thinner "squiggle"
+    squiggle02();//thicker white "squiggle"
     }
 
 
 function squiggle01(){
     push();
     noFill();
-    stroke(random(100, 300), 100, 100);
+    stroke(random(100, 300), 100, 100);//blues, greens, purple side of colour wheel
     beginShape();
     for (let x = 0; x < 10; x++){
         for(let y = 0; y < 10; y++){
@@ -29,14 +29,14 @@ function squiggle01(){
     pop();
 }
 
-function squiggle02(){
+function squiggle02(){ 
     push();
     noFill();
     stroke(255);
-    strokeWeight(3);
+    strokeWeight(3);//thicker shape stroke
     beginShape();
     for (let x = 0; x < 5; x++){
-        for(let y = 0; y < 5; y++){
+        for(let y = 0; y < 5; y++){ 
     vertex(random(width), random(height));
     endShape();        }
     }
