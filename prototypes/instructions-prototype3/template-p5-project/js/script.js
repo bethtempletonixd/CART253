3e125b4f670386a84a8963dfe961439634b3a1db
bethtempletonixd <bethtempleton99@gmail.com>
2026-09-22 12:49:30 -0400

    started prototype 3

--- prototypes/instructions-prototype3/template-p5-project/js/script.js
+++ prototypes/instructions-prototype3/template-p5-project/js/script.js
@@ -1,24 +1,22 @@
-/**
- * Title of Project
- * Author Name
- * 
- * HOW EMBARRASSING! I HAVE NO DESCRIPTION OF MY PROJECT!
- * PLEASE REMOVE A GRADE FROM MY WORK IF IT'S GRADED!
- */
+const grid = 50;
 
-"use strict";
 
-/**
- * OH LOOK I DIDN'T DESCRIBE SETUP!!
-*/
 function setup() {
-
+createCanvas(500, 500);
+background(0);
+colorMode(HSB);
 }
 
 
-/**
- * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
-*/
 function draw() {
+squareGrid();
+}
 
+function squareGrid(){
+    fill(255);
+for (let y = 0; y < 10; y++){
+    for (let x = 0; x < 10; x++){
+        square(grid + x, grid + y, 20);
+    }
+}
 }
