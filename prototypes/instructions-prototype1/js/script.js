c4f895682eb4662a32b97405a6e7062d5edc8688
bethtempletonixd <bethtempleton99@gmail.com>
2026-09-22 12:16:24 -0400

    startedRectGrid

--- prototypes/instructions-prototype1/js/script.js
+++ prototypes/instructions-prototype1/js/script.js
@@ -1,24 +1,12 @@
-/**
- * Title of Project
- * Author Name
- * 
- * HOW EMBARRASSING! I HAVE NO DESCRIPTION OF MY PROJECT!
- * PLEASE REMOVE A GRADE FROM MY WORK IF IT'S GRADED!
- */
 
-"use strict";
-
-/**
- * OH LOOK I DIDN'T DESCRIBE SETUP!!
-*/
 function setup() {
-
+createCanvas(500, 500);
+background(0);
 }
 
 
-/**
- * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
-*/
-function draw() {
 
+function draw() {
+fill(255);
+rect(width/2, height/2, 10, 30);
 }
