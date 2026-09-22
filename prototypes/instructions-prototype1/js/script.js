8c79d20a2b1e54411f8f33d39edfc8a887383b39
bethtempletonixd <bethtempleton99@gmail.com>
2026-09-22 12:28:57 -0400

    squareGrid

--- prototypes/instructions-prototype1/js/script.js
+++ prototypes/instructions-prototype1/js/script.js
@@ -1,3 +1,5 @@
+const grid = 50;
+const inset = 0 + grid/2;
 
 function setup() {
 createCanvas(500, 500);
@@ -8,5 +10,10 @@ background(0);
 
 function draw() {
 fill(255);
-rect(width/2, height/2, 10, 30);
+rectMode(CENTER);
+for (let y = 0; y < 10; y++){
+    for(let x = 0; x < 10; x++){
+rect(inset + x * grid, inset + y * grid, 10, 10);
+}
+}
 }
