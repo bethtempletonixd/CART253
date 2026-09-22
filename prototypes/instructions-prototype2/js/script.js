c449aa6474120aa108612de70ff8bc7ddbd0d27a
bethtempletonixd <bethtempleton99@gmail.com>
2026-09-22 16:23:59 -0400

    changed colour

--- prototypes/instructions-prototype2/js/script.js
+++ prototypes/instructions-prototype2/js/script.js
@@ -7,9 +7,14 @@ noLoop();
 
 
 function draw() {
-//background(0);
+background(0);
 for (let i = 0; i < 150; i++){
-    fill(255);
+    noStroke();
+    fill(random(255)); //different shades of white
 rect(random(width), random(height), 10, 30);
 }
+}
+
+function mousePressed(){
+    redraw(); //everytime the mouse is clicked the graphics on screen change
 }
