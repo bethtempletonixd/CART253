e106b5b9967b45db800a4c3cc350c162428caa3c
bethtempletonixd <bethtempleton99@gmail.com>
2026-09-22 15:49:48 -0400

    squiggle updated

--- prototypes/instructions-prototype3/template-p5-project/js/script.js
+++ prototypes/instructions-prototype3/template-p5-project/js/script.js
@@ -1,27 +1,29 @@
-const grid = 30;
-const inset = 0 + grid/2;
+// const grid = 30;
+// const inset = 0 + grid/2;
 
 
 function setup() {
 createCanvas(500, 500);
-background(0);
+//background(0);
 colorMode(HSB);
+noLoop();
 }
 
 
 function draw() {
+    background(0);
     squiggle();
-squareGrid();
+//squareGrid();
 }
 
-function squareGrid(){
-    rectMode(CENTER);
-    fill(255);
-for (let y = 0; y < 20; y++){
-    for (let x = 0; x < 25; x++){
-        square(inset + x * grid, inset + y * grid, 20);
-    }
-}
+// function squareGrid(){
+//     rectMode(CENTER);
+//     fill(255);
+// for (let y = 0; y < 20; y++){
+//     for (let x = 0; x < 25; x++){
+//         square(inset + x * grid, inset + y * grid, 20);
+//     }
+// }
 
 function squiggle(){
     noFill();
@@ -30,8 +32,8 @@ function squiggle(){
     for (let x = 0; x < 10; x++){
         for(let y = 0; y < 10; y++){
     vertex(random(width), random(height));
-    endShape(CLOSE);
+    endShape(PATH);
         }
     }
 }
-}
+//}
